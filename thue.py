bf975d804ae82ca9e46e8861da3933ef1938fc82
abnuo <66645994+abnuo@users.noreply.github.com>
2022-11-07 17:39:38 +0000

    Update thue.py

--- thue.py
+++ thue.py
@@ -1,24 +1,5 @@
 import random
 
-def run(rules,init=None):
-  rules = rules.strip().split("\n")
-  if not init:
-    init = "\n".join([i for i in rules if not "::=" in i])
-  rules = [i for i in rules if "::=" in i and not i == "::="]
-  shuffled = rules
-  output = init
-  random.shuffle(shuffled)
-  print(shuffled)
-  for i in shuffled:
-    lhs = i.split("::=")[0]
-    rhs = i.split("::=")[1]
-    print(lhs,rhs)
-    if rhs.startswith("~"):
-      output = lhs
-      rhs = rhs[1:]
-    output = output.replace(lhs,run("\n".join(rules+["::="]+init.split("\n")),rhs))
-  return output
-
 class VM:
   def __init__(self,rules,init=None):
     rules = rules.strip().split("\n")
